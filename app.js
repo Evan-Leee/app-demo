77526ad3aef8ba49748395e3d5fe3c1439c606b3
Li Yu <evanli@thoughtworks.com>
2018-01-04 15:02:25 +0800

    change the port to 9000

--- app.js
+++ app.js
@@ -34,6 +34,6 @@ app.use((err, req, res, next) => {
   res.end();
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(9000, () => console.log('Example app listening on port 9000!'));
 
 module.exports = app;
